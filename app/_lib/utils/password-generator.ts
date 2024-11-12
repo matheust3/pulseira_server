c3aa93b4a61840128da217e794a4b9c8a1974bba
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-12 22:42:54 +0000

    feat: implement password generator utility and corresponding tests

---
+++ app/_lib/utils/password-generator.ts
@@ -0,0 +1,8 @@
+export const generatePassword = (length: number): string => {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return password;
+};
