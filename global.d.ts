d4103b64732dda4a3dad2567e4cae59f08f80e54
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-12 23:17:19 +0000

    feat: add COMMUNICATION_EMAIL_DOMAIN to ProcessEnv interface in global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,16 +1,17 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: 'development' | 'production' | 'test'
-      JWT_SECRET: string
-      JWT_ISSUER: string
-      JWT_AUDIENCE: string
-      S3_ACCESS_KEY: string
-      S3_SECRET_KEY: string
-      S3_ENDPOINT: string
-      S3_BUCKET_NAME: string
-      S3_REGION: string
+      NODE_ENV: "development" | "production" | "test";
+      JWT_SECRET: string;
+      JWT_ISSUER: string;
+      JWT_AUDIENCE: string;
+      S3_ACCESS_KEY: string;
+      S3_SECRET_KEY: string;
+      S3_ENDPOINT: string;
+      S3_BUCKET_NAME: string;
+      S3_REGION: string;
+      COMMUNICATION_EMAIL_DOMAIN: string;
     }
   }
 }
-export {} // Export an empty object to ensure this file is treated as a module
+export {}; // Export an empty object to ensure this file is treated as a module
