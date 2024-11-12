701a7c6b1f2dae18920caa5d605ff4bf4f9e4f94
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-12 23:35:36 +0000

    feat: implement updatePassword method in UserRepositoryImpl with error handling and tests

---
+++ app/_lib/infra/repositories/user-repository-impl.test.ts
@@ -0,0 +1,42 @@
+import { DeepMockProxy, mock, mockDeep } from "jest-mock-extended";
+import { UserRepository } from "../../core/application/repositories/user-repository";
+import { UserRepositoryImpl } from "./user-repository-impl";
+import { PrismaClient, User } from "@prisma/client";
+import { UserNotFoundError } from "../../core/domain/errors/user-not-found-error";
+
+describe("user-repository-impl.test.ts - updatePassword", () => {
+  let sut: UserRepository;
+  let prisma: DeepMockProxy<PrismaClient>;
+
+  beforeEach(() => {
+    prisma = mockDeep<PrismaClient>();
+    sut = new UserRepositoryImpl({ prisma });
+
+    prisma.user.findUnique.mockResolvedValue(mock<User>());
+  });
+
+  test("ensure call prisma with correct params", async () => {
+    //! Arrange
+    const email = "test@example.com";
+    const password = "newPassword123";
+
+    //! Act
+    await sut.updatePassword(email, password);
+
+    //! Assert
+    expect(prisma.user.update).toHaveBeenCalledWith({
+      where: { email },
+      data: { password },
+    });
+  });
+
+  test("throws UserNotFoundError if user does not exist", async () => {
+    //! Arrange
+    const email = "nonexistent@example.com";
+    const password = "newPassword123";
+    prisma.user.findUnique.mockResolvedValue(null);
+
+    //! Act & Assert
+    await expect(sut.updatePassword(email, password)).rejects.toThrow(UserNotFoundError);
+  });
+});
---
+++ app/_lib/infra/repositories/user-repository-impl.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { UserRepository } from "../../core/application/repositories/user-repository";
+import { UserNotFoundError } from "../../core/domain/errors/user-not-found-error";
+
+export class UserRepositoryImpl implements UserRepository {
+  private readonly prisma: PrismaClient;
+
+  constructor(args: { prisma: PrismaClient }) {
+    this.prisma = args.prisma;
+  }
+
+  async updatePassword(email: string, password: string): Promise<void> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+    await this.prisma.user.update({
+      where: { email },d
+      data: { password },
+    });
+  }
+}
