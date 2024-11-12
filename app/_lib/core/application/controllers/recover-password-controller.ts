1d84d7ee82c27da0e79ffa3dad97218e061122bf
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-12 21:59:56 +0000

    chore: create recover-password-controller

---
+++ app/_lib/core/application/controllers/recover-password-controller.ts
@@ -0,0 +1,7 @@
+import { ApiResponse } from '../../domain/models/routes/api-response'
+import { Request } from '../../domain/models/routes/request'
+import { ApiController } from './api-controller'
+
+export interface RecoverPasswordController extends ApiController {
+  post?(req: Request, res: ApiResponse): Promise<void>
+}
