889de3766a4e04fde51b5b356f65a2cf8d2e8fdb
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-15 16:36:56 +0000

    feat: add LoginRateLimiter middleware to limit login attempts

---
+++ middlewares/login-rate-limiter.ts
@@ -0,0 +1,35 @@
+import { ApiResponse } from "@/app/_lib/core/domain/models/routes/api-response";
+import { Middleware } from "@/app/_lib/core/domain/models/routes/middleware";
+import { Request } from "@/app/_lib/core/domain/models/routes/request";
+import { RateLimiterMemory, RateLimiterRes } from "rate-limiter-flexible";
+
+export class LoginRateLimiter implements Middleware {
+  private readonly rateLimiter: RateLimiterMemory;
+
+  constructor() {
+    this.rateLimiter = new RateLimiterMemory({
+      points: 1,
+      duration: 1, // 1 second
+    });
+  }
+
+  async handler(request: Request, response: ApiResponse, next: () => void): Promise<void> {
+    try {
+      await this.rateLimiter.consume(request.remoteAddress.ip ?? "no-ip", 1, {});
+      next();
+    } catch (e) {
+      if (e instanceof RateLimiterRes) {
+        response.body = { error: "Too many requests" };
+        response.status = 429;
+        response.headers.set("Retry-After", Math.ceil(e.msBeforeNext / 1000).toString());
+        response.headers.set("X-RateLimit-Limit", "10");
+        response.headers.set("X-RateLimit-Remaining", e.remainingPoints.toString());
+        response.headers.set("X-RateLimit-Reset", (e.msBeforeNext / 1000).toFixed(0).toString());
+      } else {
+        response.body = { error: "Internal server error" };
+        response.status = 500;
+        console.error(e);
+      }
+    }
+  }
+}
