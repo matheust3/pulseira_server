ee988062e5ae77434977beb405b12323e11a870a
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-24 17:54:05 +0000

    feat: enhance organization creation with validation and error handling

--- app/_lib/adapters/controllers/organization/organization-controller-impl.ts
+++ app/_lib/adapters/controllers/organization/organization-controller-impl.ts
@@ -1,7 +1,11 @@
 import { OrganizationController } from "@/app/_lib/core/application/controllers/organization/organization-controller";
 import { OrganizationRepository } from "@/app/_lib/core/application/repositories/organization-repository";
+import { InvalidJsonError } from "@/app/_lib/core/domain/errors/invalid-json-error";
+import { Organization } from "@/app/_lib/core/domain/models/organization";
 import { ApiResponse } from "@/app/_lib/core/domain/models/routes/api-response";
 import { Request } from "@/app/_lib/core/domain/models/routes/request";
+import { organizationValidator } from "@/app/_lib/utils/validators/organization-validator";
+import { ValidationError } from "yup";
 
 export class OrganizationControllerImpl implements OrganizationController {
   private readonly organizationRepository: OrganizationRepository;
@@ -32,9 +36,29 @@ export class OrganizationControllerImpl implements OrganizationController {
       res.body = { message: "Unauthorized" };
     } else {
       if (req.authorization.user.permissions.manageOrganizations) {
-        // const organization = await this.organizationRepository.create(req.body);
-        res.status = 201;
-        // res.body = organization;
+        try {
+          const body = (await req.json()) as { organization: Organization };
+          const validOrganization = await organizationValidator.validate(body.organization);
+          try {
+            await this.organizationRepository.getByCnpj(validOrganization.cnpj);
+            res.status = 409;
+            res.body = { error: "Organization with this CNPJ already exists" };
+          } catch (error) {
+            const organization = await this.organizationRepository.create(validOrganization);
+            res.status = 201;
+            res.body = { organization };
+          }
+        } catch (error) {
+          if (error instanceof InvalidJsonError) {
+            res.status = 400;
+            res.body = { error: "Invalid JSON" };
+          } else if (error instanceof ValidationError) {
+            res.status = 400;
+            res.body = { error: error.message };
+          } else {
+            throw error;
+          }
+        }
       } else {
         res.status = 403;
         res.body = { message: "Forbidden" };
