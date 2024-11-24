b3c1fa4c148548c3d040554381645cf7bf8fc563
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-24 18:07:49 +0000

    feat: enhance organization update with validation, error handling, and CNPJ uniqueness check

--- app/_lib/adapters/controllers/organization/organization-controller-impl.ts
+++ app/_lib/adapters/controllers/organization/organization-controller-impl.ts
@@ -71,12 +71,50 @@ export class OrganizationControllerImpl implements OrganizationController {
       res.status = 401;
       res.body = { message: "Unauthorized" };
     } else {
-      if (req.authorization.user.permissions.manageOrganizations) {
-        // await this.organizationRepository.update(req.body);
-        res.status = 204;
-      } else {
-        res.status = 403;
-        res.body = { message: "Forbidden" };
+      try {
+        const body = (await req.json()) as { organization: Organization };
+        const validOrganization = await organizationValidator.validate(body.organization);
+
+        // Se posso gerenciar organizações ou se posso gerenciar a minha organização
+        if (
+          req.authorization.user.permissions.manageOrganizations ||
+          (req.authorization.user.permissions.manageOrganization &&
+            req.authorization.user.organization.id === validOrganization.id)
+        ) {
+          // Se eu só posso gerenciar a minha propria organização
+          if (!req.authorization.user.permissions.manageOrganizations) {
+            // não posso alterar o CNPJ
+            validOrganization.cnpj = req.authorization.user.organization.cnpj;
+          }
+          // Verifica se o cnpj já existe
+          let org: Organization | undefined;
+          try {
+            org = await this.organizationRepository.getByCnpj(validOrganization.cnpj);
+          } catch (error) {}
+
+          // Se o cnpj já existe e não é o mesmo da organização que estou tentando alterar
+          if (org && org.id !== validOrganization.id) {
+            res.status = 409;
+            res.body = { error: "Organization with this CNPJ already exists" };
+          } else {
+            const organization = await this.organizationRepository.update(validOrganization);
+            res.status = 200;
+            res.body = organization;
+          }
+        } else {
+          res.status = 403;
+          res.body = { message: "Forbidden" };
+        }
+      } catch (error) {
+        if (error instanceof InvalidJsonError) {
+          res.status = 400;
+          res.body = { error: "Invalid JSON" };
+        } else if (error instanceof ValidationError) {
+          res.status = 400;
+          res.body = { error: error.message };
+        } else {
+          throw error;
+        }
       }
     }
   }
