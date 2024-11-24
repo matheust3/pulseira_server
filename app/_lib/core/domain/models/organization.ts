f4ef9be808321551f106d75e011403f084a9d664
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-24 14:22:08 +0000

    feat: enhance Organization model with additional fields and validation rules

--- app/_lib/core/domain/models/organization.ts
+++ app/_lib/core/domain/models/organization.ts
@@ -1,4 +1,13 @@
 export interface Organization {
   id: string;
   name: string;
+  cnpj: string;
+  phone: string;
+  email: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+  isArchived: boolean;
 }
--- app/_lib/utils/validators/organization-validator.ts
+++ app/_lib/utils/validators/organization-validator.ts
@@ -1,8 +1,17 @@
-import { object, ObjectSchema, string } from "yup";
+import { boolean, object, ObjectSchema, string } from "yup";
 import { Organization } from "../../core/domain/models/organization";
 import { uuidV7 } from "./uuid-v7-validator";
 
 export const organizationValidator: ObjectSchema<Organization> = object({
   id: uuidV7.required(),
   name: string().required(),
+  cnpj: string().matches(/^\d+$/, "cnpj must contain only numbers").required(),
+  phone: string().matches(/^\d+$/, "phone must contain only numbers").required(),
+  email: string().email().required(),
+  address: string().required(),
+  city: string().required(),
+  state: string().required(),
+  zip: string().matches(/^\d+$/, "cnpj must contain only numbers").required(),
+  country: string().required(),
+  isArchived: boolean().required(),
 });
