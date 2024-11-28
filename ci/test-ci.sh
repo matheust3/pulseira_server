1fa8fe7ac3acbb44685cefc03d783701ee600676
matheus toniolli <matheus.toniolli@hotmail.com>
2024-11-28 00:23:24 +0000

    feat: update CI scripts for testing and remove pre-commit hook

---
+++ ci/test-ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# export DATABASE_URL for the test
+export DATABASE_URL=$CI_DATABASE_URL
+# Run docker-compose up in detached mode
+docker compose -f ./ci/docker-compose.yml up -d
+# Generate prisma client
+npx prisma generate
+# Push the schema to the database
+npx prisma db push
+# Run the tests
+jest --config jest.config.ci.js --silent --runInBand
+# Stop the docker-compose services
+docker compose -f ./ci/docker-compose.yml down
